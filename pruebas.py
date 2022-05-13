12639ba258b4ab0ef3ecb03cb88ac443e22c7994
Miguel Angel Pozo Morales <djmikelp@gmail.com>
2022-05-13 19:05:01 -0400

    Pruebas3

--- pruebas.py
+++ pruebas.py
@@ -27,28 +27,36 @@ wav2 =wave.open("audio2.wav","w")
 wav2.setnchannels(wav.getnchannels())
 wav2.setsampwidth(wav.getsampwidth())
 wav2.setframerate(wav.getframerate())
-#print(np.size(raw))
-print("*** tal cual ***")
-for i in range(5):
-    print(raw[i])
+
+print("*** raw ***", type(raw))
+# for i in range(1000):
+#     raw5[i]=raw[i]
 
 raw = np.frombuffer(raw, dtype=np.int16)
-raw3 = raw*1.0/(max(abs(raw))) 
-raw3 = expansion(raw3,255)
-#print("signo ",raw3)
-print("*** normalizado ***")
+print("*** rawBufer ***", type(raw))
 for i in range(5):
-    print(sgn(raw3[i]))
-wav2.writeframes(np.frombuffer(raw3, dtype=np.int16))
+    print(raw[i])
+print("tipo raw",type(raw))
+raw3 = raw*1.0/(max(abs(raw))) 
+print("*** rawNormalizado ***", type(raw3))
+for i in range(5):
+    print(raw3[i])
+raw4 = expansion(raw3,255)
+#print("tipo raw4 ",type(raw4))
+#print("raw3 ",raw3)
+print("*** raw4 ***", type(raw4))
+for i in range(5):
+    print(raw4[i])
+wav2.writeframes(raw)
 sampleRate = wav.getframerate()
 
 if wav.getnchannels() ==2:
     print("Estereo 2 canales")
     sys.exit(0)
-Time = np.linspace(0,len(raw)/sampleRate, num = len(raw))
+Time = np.linspace(0,len(raw4)/sampleRate, num = len(raw4))
 plt.title("Wveform of Wave File")
-plt.plot(Time,raw, color="blue")
+plt.plot(Time,raw4, color="red")
 plt.ylabel("Amplitud")
 plt.xlabel("Tiempo")
-#plt.show()
-print(type(raw[1]))
+plt.show()
+#print(type(raw[1]))
